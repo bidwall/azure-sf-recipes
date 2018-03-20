7ff0b811c7e619dcbca60795cc823734d878d454
Palvinder Bidwall <pal@bidwall.co.uk>
2018-03-20 20:58:35 +0000

    added recipe domain and repository

---
+++ src/Recipes.Catalog/IRecipeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Recipes.Catalog.Domain;
+
+namespace Recipes.Catalog
+{
+    public interface IRecipeRepository
+    {
+        Task<IEnumerable<Recipe>> GetAllRecipes();
+        Task AddRecipe(Recipe recipe);
+    }
+}
---
+++ src/Recipes.Catalog/RecipeRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using Recipes.Catalog.Domain;
+
+namespace Recipes.Catalog
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public RecipeRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        public async Task<IEnumerable<Recipe>> GetAllRecipes()
+        {
+            var recipes = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Recipe>>("recipes");
+            var result = new List<Recipe>();
+
+            using (var tx = _stateManager.CreateTransaction())
+            {
+                var allRecipes = await recipes.CreateEnumerableAsync(tx, EnumerationMode.Unordered);
+
+                using (var enumerator = allRecipes.GetAsyncEnumerator())
+                {
+                    while (await enumerator.MoveNextAsync(CancellationToken.None))
+                    {
+                        KeyValuePair<Guid, Recipe> current = enumerator.Current;
+                        result.Add(current.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public async Task AddRecipe(Recipe recipe)
+        {
+            var recipes = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Recipe>>("recipes");
+
+            using (var tx = _stateManager.CreateTransaction())
+            {
+                await recipes.AddOrUpdateAsync(tx, recipe.Id, recipe, (guid, value) => recipe = value);
+                await tx.CommitAsync();
+            }
+        }
+    }
+}

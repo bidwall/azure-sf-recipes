4d878dbdeaac9a65f8fe00f2461cd3a647a2cd4f
Palvinder Bidwall <pal@bidwall.co.uk>
2018-03-21 20:39:34 +0000

    create asp.net core stateless service

---
+++ Recipes.API/Controllers/RecipesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipes.API.Model;
+
+namespace Recipes.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipesController : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<ApiRecipe>> Get()
+        {
+            var apiRecipe = new ApiRecipe()
+            {
+                Id = Guid.NewGuid(),
+                Description = "description",
+                Name = "name",
+                Servings = 1
+            };
+
+            return new[] { apiRecipe };
+        }
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]ApiRecipe value)
+        {
+
+        }
+
+        // PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Recipes.API/Model/ApiRecipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Recipes.API.Model
+{
+    public class ApiRecipe
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("servings")]
+        public int Servings { get; set; }
+    }
+}
